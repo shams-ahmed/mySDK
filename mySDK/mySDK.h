16433587343364b2a1da29e69a0a16a44c58288a
Shams Ahmed <shamsahmed@me.com>
2020-02-26 16:42:12 +0000

    test

---
+++ mySDK/mySDK.h
@@ -0,0 +1,13 @@
+//
+//  mySDK.h
+//  mySDK
+//
+//  Created by Shams Ahmed on 26/02/2020.
+//  Copyright © 2020 io.bitrise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mySDK : NSObject
+
+@end
---
+++ mySDK/mySDK.m
@@ -0,0 +1,13 @@
+//
+//  mySDK.m
+//  mySDK
+//
+//  Created by Shams Ahmed on 26/02/2020.
+//  Copyright © 2020 io.bitrise. All rights reserved.
+//
+
+#import "mySDK.h"
+
+@implementation mySDK
+
+@end
